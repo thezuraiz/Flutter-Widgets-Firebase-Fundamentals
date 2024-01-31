9648a8e82844d837f3c1c6399caf85555f6920b1
Zuraiz Khan <137849379+thezuraiz@users.noreply.github.com>
2024-01-31 05:37:57 +0500

    ListViewBuilder.dart

--- lib/Widgets/ListViewBuilder.dart
+++ lib/Widgets/ListViewBuilder.dart
@@ -1,38 +1,35 @@
 import 'package:flutter/material.dart';
 
-class ListViewBuilder extends StatefulWidget {
-  const ListViewBuilder({super.key});
+class Listviewbuilder extends StatelessWidget {
+  const Listviewbuilder({super.key});
 
-  @override
-  State<ListViewBuilder> createState() => _ListViewBuilderState();
-}
-
-class _ListViewBuilderState extends State<ListViewBuilder> {
-  var item = ["First","Second","Third","Forth","Forth"];
   @override
   Widget build(BuildContext context) {
+    var fruitsInventory = {
+      "fruits": ['Orange', 'Apple', 'Banana', 'Mango'],
+      "retailer": ['Shop-1', 'Shop-2', 'Shop-3', 'Shop-4']
+    };
+
     return Scaffold(
-      appBar: AppBar(title: Text("List View Builder"),
-      ),
-      body: Container(
-        child: ListView.builder(
-          itemCount: item.length,
-          itemBuilder: (context,index){
-            return ListTile(
-              leading: Text("$index",style: TextStyle(fontWeight: FontWeight.w500,fontSize: 16),
-              ),
-              title: TextField(),
-              trailing: IconButton(
-                  icon: Icon(Icons.remove_circle_outline),
-              onPressed: (){
-                  item.removeAt(index);
-                  setState(() {});
-              },
-              ),
-            );
-          },
+        appBar: AppBar(
+          title: Text(
+            "List View Builder Practice",
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 22),
+          ),
+          centerTitle: true,
         ),
-        ),
-    );
+        body: Container(
+          alignment: Alignment.center,
+          child: ListView.builder(
+            itemCount: fruitsInventory['fruits']!.length,
+            itemExtent: 50, // To make spaces each return
+            itemBuilder: (context, index) {
+              return ListTile(
+                  title: Text("${index + 1}- ${fruitsInventory['fruits']![index]}"),
+                  subtitle: Text(fruitsInventory['retailer']![index]),
+              );
+            },
+          ),
+        ));
   }
 }
