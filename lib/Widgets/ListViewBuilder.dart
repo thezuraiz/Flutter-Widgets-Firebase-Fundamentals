e301c0466f91579a43029f260c61826d2d19cf72
Zuraiz Khan <137849379+thezuraiz@users.noreply.github.com>
2024-01-31 05:49:06 +0500

    ListViewBuilder.dart

--- lib/Widgets/ListViewBuilder.dart
+++ lib/Widgets/ListViewBuilder.dart
@@ -1,14 +1,19 @@
 import 'package:flutter/material.dart';
 
-class Listviewbuilder extends StatelessWidget {
-  const Listviewbuilder({super.key});
+class Listviewbuilder extends StatefulWidget {
+
+  @override
+  State<Listviewbuilder> createState() => _ListviewbuilderState();
+}
+
+class _ListviewbuilderState extends State<Listviewbuilder> {
+  var fruitsInventory = {
+    "fruits": ['Orange', 'Apple', 'Banana', 'Mango'],
+    "retailer": ['Shop-1', 'Shop-2', 'Shop-3', 'Shop-4']
+  };
 
   @override
   Widget build(BuildContext context) {
-    var fruitsInventory = {
-      "fruits": ['Orange', 'Apple', 'Banana', 'Mango'],
-      "retailer": ['Shop-1', 'Shop-2', 'Shop-3', 'Shop-4']
-    };
 
     return Scaffold(
         appBar: AppBar(
@@ -22,11 +27,23 @@ class Listviewbuilder extends StatelessWidget {
           alignment: Alignment.center,
           child: ListView.builder(
             itemCount: fruitsInventory['fruits']!.length,
-            itemExtent: 50, // To make spaces each return
+            // itemExtent: 50, // To make spaces each return
             itemBuilder: (context, index) {
-              return ListTile(
-                  title: Text("${index + 1}- ${fruitsInventory['fruits']![index]}"),
-                  subtitle: Text(fruitsInventory['retailer']![index]),
+              return Card(
+                child: ListTile(
+                  leading: Icon(Icons.person),
+                    title: Text(fruitsInventory['fruits']![index]),
+                    subtitle: Text(fruitsInventory['retailer']![index]),
+                  trailing: IconButton(
+                    onPressed: (){
+                      fruitsInventory['fruits']!.removeAt(index);
+                      fruitsInventory['retailer']!.removeAt(index);
+                      print('Deleted');
+                      setState(() {});
+                    },
+                    icon: Icon(Icons.delete),
+                  ),
+                ),
               );
             },
           ),
