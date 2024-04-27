585c0fec5d279f7157f5a95af4b0c50ccf6c4a23
Zuraiz Khan <137849379+thezuraiz@users.noreply.github.com>
2024-04-27 19:09:13 +0500

    CustomTabBar.dart

--- lib/Widgets/CustomTabBar.dart
+++ lib/Widgets/CustomTabBar.dart
@@ -11,27 +11,59 @@ class _CustomTabWidState extends State<CustomTabWid> {
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
-        length: 3,
-        child: Scaffold(
-          body: Padding(
-            padding: EdgeInsets.all(8.0),
-            child: Expanded(
-              child: TabBarView(
-                children: [
-                  Center(
-                    child: Text("First"),
-                  ),
-                  Center(
-                    child: Text("Second"),
-                  ),
-                  Center(
-                    child: Text("Third"),
-                  ),
-                ],
-              ),
-            )
+    length: 5,
+    child: Scaffold(
+      appBar: AppBar(
+        title: Text("Your App Title"),
+        bottom: const TabBar(
+          isScrollable: true,
+          labelColor: Colors.black,
+          unselectedLabelColor: Colors.grey,
+          indicatorColor: Colors.green,
+          tabs: [
+            Tab(text: "Khad"),
+            Tab(text: "Uranimum"),
+            Tab(text: "Photone"),
+            Tab(text: "Uric"),
+            Tab(text: "Potisium"),
+          ],
+        ),
+      ),
+      body: TabBarView(
+        children: [
+          // Content for Khad tab
+          Container(
+            child: Center(
+              child: Text("Khad Content"),
+            ),
           ),
-        )
-    );
+          // Content for Uranimum tab
+          Container(
+            child: Center(
+              child: Text("Uranimum Content"),
+            ),
+          ),
+          // Content for Photone tab
+          Container(
+            child: Center(
+              child: Text("Photone Content"),
+            ),
+          ),
+          // Content for Uric tab
+          Container(
+            child: Center(
+              child: Text("Uric Content"),
+            ),
+          ),
+          // Content for Potisium tab
+          Container(
+            child: Center(
+              child: Text("Potisium Content"),
+            ),
+          ),
+        ],
+      ),
+    ),
+  );
   }
 }
